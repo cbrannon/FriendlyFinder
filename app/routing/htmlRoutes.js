8c02d36ec016c92aa088b40a829a03e68e1bbd00
Brannon <E9879847@eaton.ad.etn.com>
2017-04-19 10:27:32 -0400

    Initial commit

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+module.exports = function(app) {
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public', 'home.html'));
+    });
+
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public', 'survey.html'));
+    });
+}
+
