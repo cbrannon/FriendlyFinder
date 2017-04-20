489a5cc68d925c08a268980573cb5b9203da6f4d
Cody Brannon <codybrannon@Codys-MacBook-Pro.local>
2017-04-19 22:20:32 -0400

    Working body-parser

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,26 +4,64 @@ const path = require("path");
 const app = express();
 const fs = require("fs");
 
+
 module.exports = function(app) {
-    app.get('/api/friends', (req, res) => {
+    // Add this line below
+    app.use(bodyParser.urlencoded({ extended: true })) 
+
+    app.use(bodyParser.json());
+
+    app.get('/api/friends', function (req, res) {
         res.sendFile(path.join(__dirname, '../data', 'friends.js'));
     });
 
     app.post('/api/friends', function (req, res) {
-        let filePath = __dirname + '../data/friends.js';
-        let requestData = "";
-        req.on('data', function(data) {
-             fs.readFile( filePath, 'utf8', (err, fileData) => {
-                let newData = JSON.parse( fileData );
-                requestData += fileData;
-                console.log(requestData);
-            });
-        });
+        
+        console.log(req.body);
 
-        // req.on('end', function (){
-        //     fs.appendFile(filePath, body, function() {
-        //         res.end();
-        //     });
+
+        // res.end(JSON.stringify(req.body));
+        // // Saving the request method as a variable.
+        // var requestData = "";
+        // // When the server receives data, it will add it to requestData.
+        // req.on("data", function(data) {
+        //     console.log(res);
+        //     requestData += data;
+        //     console.log("You just posted some data to the server!");
+        //     console.log("Your data was " + requestData);
+        // });
+
+        // // When the request has ended...
+        // req.on("end", function() {
+        //     res.write("<html><head><title>Hello Noder!</title></head><body>");
+        //     res.write("<h1>Thank You!</h1>");
+        //     res.write("</body></html>");
+        //     res.end();
         // });
     });
+
+    
+
+    // app.get('/api/friends', (req, res) => {
+    //     res.sendFile(path.join(__dirname, '../data', 'friends.js'));
+    // });
+
+    // app.post('/api/friends', function (req, res) {
+    //     let filePath = '../data/friends.js';
+    //     let requestData = "";
+    //     req.on('data', function(data) {
+    //         // console.log(data);
+    //          fs.readFile( filePath, 'utf8', (err, fileData) => {
+    //             let newData = JSON.parse( fileData );
+    //             requestData += fileData;
+    //             // console.log(requestData);
+    //         });
+    //     });
+
+    //     // req.on('end', function (){
+    //     //     fs.appendFile(filePath, body, function() {
+    //     //         res.end();
+    //     //     });
+    //     // });
+    // });
 }
