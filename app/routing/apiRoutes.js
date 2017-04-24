801e54743e2c659b81e47d22536e8d645b5d480d
Cody Brannon <codybrannon@Codys-MacBook-Pro.local>
2017-04-23 20:53:21 -0400

    Find closest match, algorithm needs adjustment

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,47 +16,42 @@ module.exports = function(app) {
     });
 
     app.post('/api/friends', function (req, res) {
+        // console.log(req.body);
         fs.readFile( path.join(__dirname, '../data', 'friends.js'), 'utf8', function (err, data) {
             let friendArray = JSON.parse(data);
+            let currentSum = req.body.scores.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+            let friendSums = [];
+            let friendMatch;
+
+            function findAllSums() {
+                friendArray.forEach(currentItem => {
+                    let currentSum = currentItem.scores.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+                    friendSums.push(currentSum);
+                });
+                findClosest(currentSum, friendSums);
+            }
+
+            function findClosest(current, friends) {
+                let closestDifference;
+                let closestIndex;
+
+                friends.forEach((currentItem, index) => {
+                    currentDifference = Math.abs(currentItem - current);
+
+                    if (closestDifference == undefined || currentDifference <= closestDifference) {
+                        closestDifference = currentDifference;
+                        closestIndex = index;
+                    }
+                });
+                console.log(closestDifference);
+                friendMatch = friendArray[closestIndex];
+            }
+
+            findAllSums();
+
             friendArray.push(req.body);
-            res.end(JSON.stringify(friendArray));
+            res.json(friendMatch);
             res.end(fs.writeFile( path.join(__dirname, '../data', 'friends.js'), JSON.stringify(friendArray)));
         });
-
-        // res.end(JSON.stringify(req.body));
-        // // Saving the request method as a variable.
-        // var requestData = "";
-        // // When the server receives data, it will add it to requestData.
-        // req.on("data", function(data) {
-        //     console.log(res);
-        //     requestData += data;
-        //     console.log("You just posted some data to the server!");
-        //     console.log("Your data was " + requestData);
-        // });
     });
-
-    
-
-    // app.get('/api/friends', (req, res) => {
-    //     res.sendFile(path.join(__dirname, '../data', 'friends.js'));
-    // });
-
-    // app.post('/api/friends', function (req, res) {
-    //     let filePath = '../data/friends.js';
-    //     let requestData = "";
-    //     req.on('data', function(data) {
-    //         // console.log(data);
-    //          fs.readFile( filePath, 'utf8', (err, fileData) => {
-    //             let newData = JSON.parse( fileData );
-    //             requestData += fileData;
-    //             // console.log(requestData);
-    //         });
-    //     });
-
-    //     // req.on('end', function (){
-    //     //     fs.appendFile(filePath, body, function() {
-    //     //         res.end();
-    //     //     });
-    //     // });
-    // });
 }
