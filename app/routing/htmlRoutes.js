95f8245a690ebd08edb7ce716260f7f3edf31a58
Cody Brannon <codybrannon@lawn-143-215-86-58.lawn.gatech.edu>
2017-04-27 21:58:43 -0400

    Update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const app = express();
 
 module.exports = function(app) {
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public', 'home.html'));
+        res.sendFile(path.join(__dirname, '..','public', 'home.html'));
     });
 
     app.get('/survey', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public', 'survey.html'));
+        res.sendFile(path.join(__dirname, '..','public', 'survey.html'));
     });
 }
 
